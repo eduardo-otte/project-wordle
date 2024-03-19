2a661720d4f28087135df9fd031b9ca74b6bc955
Eduardo Otte <otte@salvy.com.br>
2024-03-19 10:26:18 -0300

    exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log(guess);
+        setGuess("");
+      }}
+    >
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        value={guess}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(event) => setGuess(event.target.value.toLocaleUpperCase())}
+        id="guess-input"
+        type="text"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
